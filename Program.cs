1f1daa30d4f1cb51589f20085738061709ca3c55
Paulo Kinjo <paulokinjo@hotmail.com>
2024-07-06 19:22:15 -0400

    Making a foreach test

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿public class ForEachTest
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+        int total = 0;
+
+        // add each element's value to total
+        foreach (int number in array)
+            total += number;
+
+        Console.WriteLine($"Total of array elements: {total}");
+    }
+}
